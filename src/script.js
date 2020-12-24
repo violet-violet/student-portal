39fd39e01138c006ba7e99761310496fae028d4b
E.Demidovskaia <violet7693@gmail.com>
2020-12-24 22:14:28 +0300

    feat: deploy statistics

--- src/script.js
+++
@@ -1,7 +0,0 @@
-import './assets/styles/normalize.css';
-import './assets/styles/style.scss';
-import people from './assets/scripts/people';
-import Portal from './assets/scripts/Portal';
-
-const studentPortal = new Portal(people);
-studentPortal.init();
